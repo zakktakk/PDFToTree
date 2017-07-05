28dd0c735fe352fa58dc2cdce892b3b2e4e14566
Takuro Yamazaki <takuro.yamazaki.w@gmail.com>
2017-07-05 13:16:44 +0900

    cleanup reason.py

--- src/editted/py2read.py
+++ src/editted/py2read.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from tqdm import tqdm
-# from cStringIO import StringIO
+from io import StringIO
 
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfparser import PDFParser
@@ -11,50 +11,46 @@ from pdfminer.converter import TextConverter
 from pdfminer.cmapdb import CMapDB
 from pdfminer.layout import LAParams
 
-# def read(path, debug=0, password='', pagenos=set(), maxpages=0, outfile=None,
-#          outtype=None, imagewriter=None, rotation=0, stripcontrol=False,
-#          layoutmode='normal', codec='utf-8', pageno=1, scale=1,
-#          caching=True, showpageno=True, laparams=LAParams()):
-#
-#         retstr = StringIO()
-#
-#         PDFDocument.debug = debug
-#         PDFParser.debug = debug
-#         CMapDB.debug = debug
-#         PDFPageInterpreter.debug = debug
-#
-#         rsrcmgr = PDFResourceManager(caching=caching)
-#         device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams,
-#                                    imagewriter=imagewriter)
-#
-#         fp = open(path, 'rb')
-#         interpreter = PDFPageInterpreter(rsrcmgr, device)
-#
-#
-#         pbar = tqdm(total=30)
-#         for number,page in enumerate(PDFPage.get_pages(fp, pagenos,
-#                                         maxpages=maxpages, password=password,
-#                                         caching=caching, check_extractable=True)):
-#                 page.rotate = (page.rotate+rotation) % 360
-#                 interpreter.process_page(page)
-#                 pbar.update(1)
-#         text = retstr.getvalue()
-#
-#         fp.close()
-#         device.close()
-#
-#         return text
+def read(path, debug=0, password='', pagenos=set(), maxpages=0, outfile=None,
+         outtype=None, imagewriter=None, rotation=0, stripcontrol=False,
+         layoutmode='normal', codec='utf-8', pageno=1, scale=1,
+         caching=True, showpageno=True, laparams=LAParams()):
+    """
+    @description get text on input pdf
+    @param path path to pdf file
+    @return text on pdf
+    """
 
-def second(path):
-    import PyPDF2
-    pdfFileObj = open(path,'rb') #'rb' for read binary mode
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        retstr = StringIO()
 
-    pageObj = pdfReader.getPage(9) #'9' is the page number
-    print(pageObj.extractText())
+        PDFDocument.debug = debug
+        PDFParser.debug = debug
+        CMapDB.debug = debug
+        PDFPageInterpreter.debug = debug
+
+        rsrcmgr = PDFResourceManager(caching=caching)
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams,
+                                   imagewriter=imagewriter)
+
+        fp = open(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        pbar = tqdm(total=30)
+        for number,page in enumerate(PDFPage.get_pages(fp, pagenos,
+                                        maxpages=maxpages, password=password,
+                                        caching=caching, check_extractable=True)):
+                page.rotate = (page.rotate+rotation) % 360
+                interpreter.process_page(page)
+                pbar.update(1)
+        text = retstr.getvalue()
+
+        fp.close()
+        device.close()
+
+        return text
 
 
 if __name__ == '__main__':
-    path = "../../inputs/pdf/decryp.pdf"
+    path = "../../inputs/pdf/4503 Astellas Pharma.pdf"
 
-    second(path)
+    print(read(path))
